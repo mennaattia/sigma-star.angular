0ff162ee8117782e4a4e3ae95148d30621cf12e1
Your Name <you@example.com>
2022-11-26 21:17:38 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { Error404Component } from './error404/error404.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "home", component : HomeComponent},
+  {path : "", component : HomeComponent},
+  {path : "about", component : AboutComponent},
+  {path: "services", component : ServicesComponent},
+  {path : "clients", component : ClientsComponent},
+  {path : "contact", component : ContactComponent},
+  {path: "**", component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ClientsComponent,
+    ContactComponent,
+    NavComponent,
+    FooterComponent,
+    ServicesComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
